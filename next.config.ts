502b58f07561446f62d7e88cd1aabf7e28d547b5
maso2k20 <usenet@maso.me.uk>
2025-08-09 21:05:40 +0100

    Updated Next Config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   eslint: {
     // Don’t fail the production build on ESLint errors
     ignoreDuringBuilds: true,
