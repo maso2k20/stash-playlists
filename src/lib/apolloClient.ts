4138a95616d9b6cbae12f2ca27b46884d800686e
maso2k20 <usenet@maso.me.uk>
2025-08-10 09:59:12 +0100

    Fixed Apollo uri

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -1,8 +1,10 @@
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
+const graphqlUrl = `${process.env.STASH_SERVER?.replace(/\/$/, '')}/graphql`;
+
 export const apolloClient = new ApolloClient({
   link: new HttpLink({
-    uri: process.env.NEXT_PUBLIC_STASH_GRAPHQL, // e.g. "/api/stash-graphql"
+    uri: graphqlUrl,
     credentials: 'same-origin',                // include cookies if you need auth
     headers: {
       // If you’re proxying via Next.js API route, you don’t need ApiKey here.
